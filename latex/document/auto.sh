fdc4253a51a47e7c5cbffd69f73bdbf9aaa24237
adarijani <ali.darijani.me@gmail.com>
2023-08-29 15:31:25 +0200

    commit

--- latex/document/auto.sh
+++ latex/document/auto.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git pull
-lualatex thesis-template.tex
+#lualatex thesis-template.tex
 #convert thesis-template.pdf thesis-template.jpg
 #convert thesis-template.jpg -resize 30%  thesis-template-30.jpg
 git add .
